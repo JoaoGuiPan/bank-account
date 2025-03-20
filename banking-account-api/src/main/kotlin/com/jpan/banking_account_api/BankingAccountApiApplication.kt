0feee2db78d6d55345d5b37967e54a1cbed4bb9b
Joao Guilherme Pan <joaoguilhermepan@gmail.com>
2025-03-20 14:27:33 +0100

    project initial setup, java 21 spring 3.4 and angular 19

---
+++ banking-account-api/src/main/kotlin/com/jpan/banking_account_api/BankingAccountApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jpan.banking_account_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankingAccountApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankingAccountApiApplication>(*args)
+}
---
+++ banking-account-api/src/test/kotlin/com/jpan/banking_account_api/BankingAccountApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jpan.banking_account_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankingAccountApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
